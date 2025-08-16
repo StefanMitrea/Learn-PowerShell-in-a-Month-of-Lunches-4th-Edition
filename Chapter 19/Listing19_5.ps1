d7bd2b843b385186f88a0377e8901e142f6ed301
StefanMitrea <mitreastefan@gmail.com>
2025-08-16 14:09:41 +0100

    update

---
+++ Chapter 19/Listing19_5.ps1
@@ -0,0 +1,29 @@
+<#
+.SYNOPSIS
+Get-DiskInventory retrieves logical disk information from one or more computers.
+
+.DESCRIPTION
+Get-DiskInventory uses CIM to retrieve the Win32_LogicalDisk instances from one or more computers.
+It displays each disk's drive letter, free space, total size, and percentage of free space.
+
+.PARAMETER computername
+The computer name, or names, to query. Default: Localhost.
+
+.PARAMETER drivetype
+The drive type to query. See Win32_LogicalDisk documentation for values. 3 is a fixed disk, and is the default.
+
+.EXAMPLE
+Get-DiskInventory -computername SRV02 -drivetype 3
+#>
+param(
+    $computername = 'localhost',
+    $drivetype = 3
+)
+
+Get-CimInstance -Class Win32_LogicalDisk -ComputerName $computername `
+  -Filter "DriveType=$drivetype" |
+Sort-Object -Property DeviceID |
+Format-Table -Property DeviceID,
+  @{label='FreeSpace(MB)';expression={$_.FreeSpace / 1MB -as [int]}},
+  @{label='Size(GB)';expression={$_.Size / 1GB -as [int]}},
+  @{label='%Free';expression={($_.FreeSpace / $_.Size * 100) -as [int]}}
